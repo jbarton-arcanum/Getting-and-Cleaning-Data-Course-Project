6fa19e7bd4a34603bef44ff06350285bcf2903c9
jbarton-arcanum <jbarton@arcanumalloys.com>
2020-04-17 15:04:47 -0400

    run_analysis.R is completed and functional README.md has been updated and describes the function BartonTidyData.txt contains the final output table per the project objective

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,8 @@
 library(qdap)
 library(data.table)
+library(tools)
+library(dplyr)
+library(stringr)
 
 
 # gets all .txt files in data directory
@@ -16,23 +19,48 @@ names <- gsub(".txt", "", basename(paths))
 
 # Loop in desired files and put them in containers
 for(i in seq_along(paths)){
-                assign(names[i], read.table(paths[i], header = F, sep = "", stringsAsFactors = F))
-                        }
+                assign(names[i],
+                       read.table(
+                               paths[i],
+                               header = F,
+                               sep = "",
+                               stringsAsFactors = F
+                               )
+                       )
+                }
 
 # Column names for data
-colabs <- c("Subject", "Activity", features$V2)
+colabs <- c("Participant", "Activity", features$V2)
 
-# combine test and train files into one table
+# Combine test and train files into one table
 testdata <- cbind(subject_test, y_test, X_test)
 traindata <- cbind(subject_train, y_train, X_train)
 
-# combine both data sets and label columns
+# Combine both data sets and label columns
 alldata <- rbind(testdata, traindata)
 colnames(alldata) <- colabs
 
-#Relabel activity index
+# Reformat activity labels and relabel activity index
+activity_labels$V2 <- toTitleCase(tolower(gsub("_", " ", activity_labels$V2)))
 alldata$Activity <- multigsub(activity_labels$V1, activity_labels$V2, alldata$Activity)
 
-# Subset mean and std data
-cols <- grep("Subject|Activity|mean|std", colnames(alldata), value = T)
-alldata <- alldata[, cols]
+# Dump unused objects
+rm(activity_labels, features, subject_test, subject_train, testdata, traindata, X_test, X_train, y_test, y_train)
+rm(all.files, colabs, i, names, paths)
+
+# Clean up column labels and subset mean and std data (Omitting meanFreq columns)
+cols <- grep("Participant|Activity|mean[()]|std", colnames(alldata), value = T)
+alldata <- as_tibble(alldata[, cols])
+cols <- gsub("BodyBody", "Body", cols)
+rm(cols)
+
+# Clean up and summarize data based on participant and activity, rename columns
+tidydata <- alldata %>%
+                group_by(Participant, Activity) %>%
+                summarize_if(is.numeric, mean) %>%
+                rename_at(vars(-(1:2)), ~ paste0(., " Average"))
+View(tidydata)
+
+# Write tidydata to hard drive for submission
+write.table(tidydata, file="BartonTidyData.txt", row.names = F)
+                                                                           
